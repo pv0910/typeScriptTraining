0abd1d93dd9aff60a1e1f7bf2daf25c1ce9b50b2
pjv0910 <19cse078@gweca.ac.in>
2023-11-21 18:44:16 +0530

    make some improvement

--- Fetch API/App.ts
+++ Fetch API/App.ts
@@ -1,29 +1,110 @@
-interface UserData {id: number;avatar: string;first_name: string;last_name: string;email: string;}
-document.addEventListener("DOMContentLoaded", async () => {
-    const userListElement = document.getElementById("userList");
-    if (!userListElement) {
+// interface UserData {id: number;avatar: string;first_name: string;last_name: string;email: string;}
+// document.addEventListener("DOMContentLoaded", async () => {
+//     const userListElement = document.getElementById("userList");
+//     if (!userListElement) {
+//       console.error("User list element not found");
+//       return;
+//     }
+//     try {
+//       const response = await fetch('https://reqres.in/api/users');
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+  
+//       const data = await response.json();
+//       const users: UserData[] = data.data;
+  
+//       users.forEach(user => {
+//         const userCard = createUserCard(user);
+//         userListElement.appendChild(userCard);
+//       });
+//     } catch (error) {
+//       console.error('Error fetching and rendering users:', error);
+//     }
+   
+//   });
+//   async function showUserDetails(userId: number) {
+//     try {
+//       const response = await fetch(`https://reqres.in/api/users/${userId}`);
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+
+//       const data = await response.json();
+//       const user: UserData = data.data;
+
+//       alert(`User Details:\nID: ${user.id}\nFirst Name: ${user.first_name}\nLast Name: ${user.last_name}\nEmail: ${user.email}`);
+//     } catch (error) {
+//       console.error('Error fetching user details:', error);
+//     }
+//   }
+//   function createUserCard(user: UserData) {
+//     const userCard = document.createElement("div");
+//     userCard.className = "user-card";
+//     userCard.innerHTML = `
+//       <img src="${user.avatar}" alt="User Image" onclick="showUserDetails(${user.id})">
+//       <p>${user.first_name} ${user.last_name}</p>
+//       <p>${user.email}</p>
+//     `;
+//     return userCard;
+//   }
+
+interface IUserData {
+  id: number;
+  avatar: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+}
+
+class UserManager {
+  private userListElement: HTMLElement | null;
+
+  constructor() {
+    this.userListElement = document.getElementById("userList");
+    if (!this.userListElement) {
       console.error("User list element not found");
       return;
     }
+  }
+
+  public async initialize() {
+    try {
+      const users = await this.fetchUsers();
+      users.forEach(user => {
+        const userCard = this.createUserCard(user);
+        this.userListElement!.appendChild(userCard);
+      });
+    } catch (error) {
+      console.error('Error fetching and rendering users:', error);
+    }
+  }
+
+  public async fetchUsers(): Promise<IUserData[]> {
     try {
       const response = await fetch('https://reqres.in/api/users');
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-  
+
       const data = await response.json();
-      const users: UserData[] = data.data;
-  
-      users.forEach(user => {
-        const userCard = createUserCard(user);
-        userListElement.appendChild(userCard);
-      });
+      return data.data;
     } catch (error) {
-      console.error('Error fetching and rendering users:', error);
+      console.error('Error fetching users:', error);
+      throw error;
     }
-   
-  });
-  async function showUserDetails(userId: number) {
+  }
+
+  public async showUserDetails(userId: number){
+    try {
+      const user = await this.fetchUserDetails(userId);
+      this.displayUserDetails(user);
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+    }
+  }
+
+  public async fetchUserDetails(userId: number): Promise<IUserData> {
     try {
       const response = await fetch(`https://reqres.in/api/users/${userId}`);
       if (!response.ok) {
@@ -31,20 +112,28 @@ document.addEventListener("DOMContentLoaded", async () => {
       }
 
       const data = await response.json();
-      const user: UserData = data.data;
-
-      alert(`User Details:\nID: ${user.id}\nFirst Name: ${user.first_name}\nLast Name: ${user.last_name}\nEmail: ${user.email}`);
+      return data.data;
     } catch (error) {
       console.error('Error fetching user details:', error);
+      throw error;
     }
   }
-  function createUserCard(user: UserData) {
+
+  public displayUserDetails(user: IUserData):void  {
+    alert(`User Details:\nID: ${user.id}\nFirst Name: ${user.first_name}\nLast Name: ${user.last_name}\nEmail: ${user.email}`);
+  }
+
+  public createUserCard(user: IUserData): HTMLDivElement {
     const userCard = document.createElement("div");
     userCard.className = "user-card";
     userCard.innerHTML = `
-      <img src="${user.avatar}" alt="User Image" onclick="showUserDetails(${user.id})">
+      <img src="${user.avatar}" alt="User Image" onclick="userManager.showUserDetails(${user.id})">
       <p>${user.first_name} ${user.last_name}</p>
       <p>${user.email}</p>
     `;
     return userCard;
   }
+}
+
+const userManager = new UserManager();
+document.addEventListener("DOMContentLoaded", () => userManager.initialize());
